a7af720e7272c9b1253ff762d2c91cbfa34916e9
Harrold Paderan <harrold@ualberta.ca>
2020-08-15 13:51:30 -0600

    road looper

---
+++ Assets/Scripts/RoadLooper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadLooper : MonoBehaviour
+{
+    public GameObject[] roadList;
+
+    public float roadMoveSpeed;
+
+    void Update() {
+        foreach (GameObject r in roadList) {
+            r.transform.Translate(new Vector3(0,0, -1 * roadMoveSpeed * Time.deltaTime));
+
+            if (r.transform.position.z <= -35) {
+                Vector3 oldPos = r.transform.position;
+                oldPos.z = 60;
+                r.transform.position = oldPos;
+            }
+        }
+    }
+}
