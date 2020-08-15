48a6f709f04c1f926d0a48ebcf573fa206488f50
Samadhi <samadhi@ualberta.ca>
2020-08-15 17:59:05 -0600

    Road collider done

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -26,22 +26,24 @@ public class CameraScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
     //if()
         //use for cat objects:
         //transform.Translate(new Vector3(0,0,cameraspeed * Time.deltaTime));
 
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(playerspeed,0,0);
-        }
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(-playerspeed,0,0);
-            //transform.Translate(new Vector3(-playerspeed * Time.deltaTime,0,0));
-        }
-     
+        // if(Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     GetComponent<Rigidbody>().velocity = new Vector3(playerspeed,0,0);
+        // }
+        // if(Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     GetComponent<Rigidbody>().velocity = new Vector3(-playerspeed,0,0);
+        //     //transform.Translate(new Vector3(-playerspeed * Time.deltaTime,0,0));
+        // }
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody>().velocity = new Vector3(playerspeed * horizontalInput,0,0);
     }
 }
 
