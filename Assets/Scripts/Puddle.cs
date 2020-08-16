4b1792077e5d285b1611beeb0f7be3eceea884ae
Harrold Paderan <harrold@ualberta.ca>
2020-08-15 19:32:43 -0600

    spawner; update on spawn items

--- Assets/Scripts/Puddle.cs
+++ Assets/Scripts/Puddle.cs
@@ -6,8 +6,19 @@ public class Puddle : MonoBehaviour, ICrashable
 {
     public float moveSpeed;
 
+    Transform carRef;
+
+    void Start()
+    {
+        carRef = GameObject.Find("Car").transform;
+    }
+
     void FixedUpdate () {
         transform.Translate(new Vector3(0,0,-1 * moveSpeed * Time.deltaTime));
+
+        if (Mathf.Abs(transform.position.z - carRef.position.z) <= 0.01) {//!!!!!!!!!!!!!!!!!!!!!!!!
+            Destroy(gameObject);
+        }
     }
 
     public void onCrash () {
--- Assets/Scripts/WetPussy.cs
+++ Assets/Scripts/WetPussy.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class WetPussy : MonoBehaviour, ICrashable
 {
     public float moveSpeed;
+    
+    Transform carRef;
+
+    void Start()
+    {
+        carRef = GameObject.Find("Car").transform;
+    }
 
     void FixedUpdate () {
         transform.Translate(new Vector3(0,0,-1 * moveSpeed * Time.deltaTime));
+
+        if (Mathf.Abs(transform.position.z - carRef.position.z) <= 0.1) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Destroy(gameObject);
+        }
     }
 
     public void onCrash () {
