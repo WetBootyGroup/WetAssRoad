2d8f5fddeec9fa141ff441bbe98160111a497d77
Allan Manuba <manuba@ualberta.ca>
2020-08-16 09:57:31 -0600

    Add background layer and moving road

---
+++ Assets/Scripts/Wet.UI/RoadParallax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Wet.UI
+{
+    public class RoadParallax : MonoBehaviour
+    {
+        public float intensity = 1f;
+        public float magnitudeMax = 5f;
+        public Transform movingReference;
+
+        private RectTransform _localTransform;
+        private float _xOffset;
+
+        private void Start()
+        {
+            Assert.IsNotNull(movingReference, "Missing moving reference in " + name);
+            _localTransform = GetComponent<RectTransform>();
+            _xOffset = movingReference.position.x;
+        }
+
+        private void Update()
+        {
+            float val = Mathf.Clamp(
+                (movingReference.position.x - _xOffset) * intensity,
+                -magnitudeMax,
+                magnitudeMax);
+            _localTransform.anchoredPosition = new Vector2(
+                 val,
+                0);
+        }
+    }
+}
